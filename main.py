87ac02eefceefff4155a7589a6bf31eaeae1ad31
ih4kim <ih4kim@edu.uwaterloo.ca>
2020-05-16 15:58:50 -0400

    Enable video feed

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import cv2 as cv2
 import numpy
 import time
-import ClosedEyeDetection
+#import ClosedEyeDetection
 
 def readFrame(video_capture):
     ret, frame = video_capture.read()
     return frame
 
-def alertSystem():
+#def alertSystem():
     #Arduino stuff
 
 if __name__ == "__main__":
     video_capture = cv2.VideoCapture(0)
     while True:
         image = readFrame(video_capture)
-        eyes_closed = CloseEyeDetection.eyeDetection(image)
+        cv2.imshow("Video", image)
+        #eyes_closed = CloseEyeDetection.eyeDetection(image)
         timer = 0
         initial_time = time.perf_counter()
+        eyes_closed = True
         while (eyes_closed and timer < 5.0):
-            image = readFrame()
-            eyes_closed = eyes_closed(image)
+            image = readFrame(video_capture)
+            cv2.imshow("Video", image)
+            #eyes_closed = CloseEyeDetection.eyeDetection(image)
             timer = time.perf_counter() - initial_time
         if (eyes_closed):
-            alertSystem()
+            pass #add arduino code
