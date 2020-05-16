42e5372902ae28cfd9b352d7924517467b4dd29d
ih4kim <ih4kim@edu.uwaterloo.ca>
2020-05-16 16:15:07 -0400

    fixed video feed

--- main.py
+++ main.py
@@ -12,17 +12,24 @@ def readFrame(video_capture):
 
 if __name__ == "__main__":
     video_capture = cv2.VideoCapture(0)
-    while True:
+    start = True
+    while (start):
         image = readFrame(video_capture)
         cv2.imshow("Video", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            start = False
+            break
         #eyes_closed = CloseEyeDetection.eyeDetection(image)
         timer = 0
-        initial_time = time.perf_counter()
+        #initial_time = time.perf_counter()
         eyes_closed = True
         while (eyes_closed and timer < 5.0):
             image = readFrame(video_capture)
             cv2.imshow("Video", image)
             #eyes_closed = CloseEyeDetection.eyeDetection(image)
-            timer = time.perf_counter() - initial_time
+            #timer = time.perf_counter() - initial_time
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                start = False
+                break
         if (eyes_closed):
             pass #add arduino code
