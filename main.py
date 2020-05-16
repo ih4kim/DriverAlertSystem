9a6ce5561505d256e9da743df8ced520999ad2ee
ih4kim <ih4kim@edu.uwaterloo.ca>
2020-05-16 14:47:28 -0400

    Update main

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import cv2 as cv2
 import numpy
 import time
+import ClosedEyeDetection
+
+def readFrame(video_capture):
+    ret, frame = video_capture.read()
+    return frame
+
+def alertSystem():
+    #Arduino stuff
 
 if __name__ == "__main__":
+    video_capture = cv2.VideoCapture(0)
     while True:
-        image = readFrame()
-        eyes_closed = eyeDetection(image)
+        image = readFrame(video_capture)
+        eyes_closed = CloseEyeDetection.eyeDetection(image)
         timer = 0
         initial_time = time.perf_counter()
         while (eyes_closed and timer < 5.0):
