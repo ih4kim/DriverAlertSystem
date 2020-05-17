5b9857c8c5bdc57b9ba8610b2547844016392745
ih4kim <ih4kim@edu.uwaterloo.ca>
2020-05-17 02:57:00 -0400

    Update main; added eye isolation function; added GUI

---
+++ EyeIsolation.py
@@ -0,0 +1,24 @@
+import cv2 as cv2
+import numpy as np
+import pdb
+
+
+def isolateEye(image):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+    faces = face_cascade.detectMultiScale(image, 1.3, 5)
+    roi_eyes = []
+    for (x,y,w,h) in faces:
+        #cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = image[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            #cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,0),2)
+            roi_eyes.append(roi_gray[ey:ey+eh, ex:ex+ew])
+            #cv2.imshow("Eye", roi_eyes[0])
+    return roi_eyes
+    
+
+
+
---
+++ main.py
@@ -0,0 +1,93 @@
+import cv2
+import sys
+import pdb
+import EyeIsolation
+from PyQt5.QtWidgets import  QWidget, QLabel, QApplication
+from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+
+cap = cv2.VideoCapture(0)
+
+class Thread(QThread):
+    changePixmapMainCamera = pyqtSignal(QImage)
+    changePixmapEye1 = pyqtSignal(QImage)
+    changePixmapEye2 = pyqtSignal(QImage)
+
+    def convertToQT(self, image):
+        height, width = image.shape
+        #bytesPerLine = width
+        ConvertToQTImage = QImage(image.data.tobytes(), width, height, QImage.Format_Grayscale8)
+        scaled = ConvertToQTImage.scaled(width, height, Qt.KeepAspectRatio)
+        return scaled
+
+    def run(self):
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+                eyeImages =  EyeIsolation.isolateEye(grayImage)
+                eyesQT = []
+                for eye in eyeImages:
+                    eyesQT.append(self.convertToQT(eye))
+                #insert eye open closed detection software
+                if (len(eyesQT)>0):
+                    self.changePixmapEye1.emit(eyesQT[0])
+                if (len(eyesQT)>1):
+                    self.changePixmapEye2.emit(eyesQT[1])
+                mainCamera = self.convertToQT(grayImage)
+                self.changePixmapMainCamera.emit(mainCamera)
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Driver Alert System'
+        self.left = 100
+        self.top = 100
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
+    @pyqtSlot(QImage)
+    def setMainCameraImage(self, image):
+        self.MainCamera.setPixmap(QPixmap.fromImage(image))
+    
+    @pyqtSlot(QImage)
+    def setEye1Image(self, image):
+        self.Eye1.setPixmap(QPixmap.fromImage(image))
+
+    @pyqtSlot(QImage)
+    def setEye2Image(self, image):
+        self.Eye2.setPixmap(QPixmap.fromImage(image))
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.resize(1800, 1200)
+        # create a label
+        self.MainCamera = QLabel(self)
+        self.MainCamera.move(100, 120)
+
+        self.Eye1 = QLabel(self)
+        self.Eye1.move(1000, 120)
+
+        self.Eye2 = QLabel(self)
+        self.Eye2.move(1000, 220)
+
+        self.MainCamera.resize(640, 480)
+        self.Eye1.resize(100, 100)
+        self.Eye2.resize(100, 100)
+
+        th = Thread(self)
+        th.changePixmapMainCamera.connect(self.setMainCameraImage)
+        th.changePixmapEye1.connect(self.setEye1Image)
+        th.changePixmapEye2.connect(self.setEye2Image)
+        th.start()
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec_())
+    cap.release()
+    cv2.destroyAllWindows()
